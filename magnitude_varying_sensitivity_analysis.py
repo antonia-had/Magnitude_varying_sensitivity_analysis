21f055f53942f572de8db7e0f8587344d5905d30
antonia-had <ah986@cornell.edu>
2019-02-25 15:08:05 -0500

    Steps

--- magnitude_varying_sensitivity_analysis.py
+++ magnitude_varying_sensitivity_analysis.py
@@ -35,6 +35,58 @@ def alpha(i, base=0.2):
 
 # Read in historical data
 histData = np.loadtxt('./historical_data.txt')
+
+# Plot historical series
+fig = plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(np.arange(len(histData)),histData, c='black')
+ax.set_ylabel('Annual shortage (af)', fontsize=12)
+ax.set_xlabel('Year on record', fontsize = 12)
+plt.savefig('historical_data.png')
+
+# Sort historical data in percentiles of magnitude
+hist_sort = np.sort(histData)
+
+# Estimate percentiles according to record length
+P = np.arange(1.,len(histData)+1)*100 / len(histData)
+
+# Plot historical percentiles of magnitude
+fig = plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(P,hist_sort, c='black')
+ax.set_ylabel('Annual shortage (af)', fontsize=12)
+ax.set_xlabel('Shortage magnitude percentile', fontsize=12)
+plt.savefig('historical_data_percentiles.png')
+
+# Read in experiment data
 expData = np.loadtxt('./experiment_data.txt')
 
+# Sort experiment data in percentiles of magnitude
+expData_sort = np.zeros_like(expData)
+for j in range(samples):
+    expData_sort[:,j] = np.sort(expData[:,j])
     
+
+# Plot historical percentiles of magnitude and sampled series        
+fig = plt.figure()
+ax=fig.add_subplot(1,1,1)
+for j in range(samples):
+    ax.plot(P,expData_sort[:,j], c = '#4286f4')
+ax.plot(P,expData_sort[:,0], c = '#4286f4', label = 'Experiment')            
+ax.plot(P,hist_sort, c='black', label = 'Historical')
+ax.legend(loc = 'upper left')
+ax.set_ylabel('Annual shortage (af)', fontsize=12)
+ax.set_xlabel('Shortage magnitude percentile', fontsize=12)
+plt.savefig('experiment_data_all.png') 
+
+# Plot range of experiment outputs
+fig = plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.fill_between(P, np.min(expData_sort[:,:],1), np.max(expData_sort[:,:], 1), color='#4286f4', alpha = 0.1, label = 'Experiment')
+ax.plot(P, np.min(expData_sort[:,:],1), linewidth=0.5, color='#4286f4', alpha = 0.3)
+ax.plot(P, np.max(expData_sort[:,:],1), linewidth=0.5, color='#4286f4', alpha = 0.3)        
+ax.plot(P,hist_sort, c='black', label = 'Historical')
+ax.legend(loc = 'upper left')
+ax.set_ylabel('Annual shortage (af)', fontsize=12)
+ax.set_xlabel('Shortage magnitude percentile', fontsize=12)
+plt.savefig('experiment_data_range.png') 
